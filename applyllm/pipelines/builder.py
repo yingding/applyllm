752b95cd340eb40ed0dd8372e3f681b68ffa1ac4
yingding <yingdingwang@yahoo.de>
2024-02-05 22:20:36 +0100

    updated override method in KwargsBuilder to express the later kwargs override the prevous kwargs during the merge/build call

--- applyllm/pipelines/builder.py
+++ applyllm/pipelines/builder.py
@@ -7,16 +7,21 @@ class KwargsBuilder:
         self.args_list = args_list
 
 
-    def append(self, kwargs: dict):
+    def override(self, kwargs: dict):
         self.args_list.append(kwargs)
         return self
 
 
-    def build(self):
+    def build(self) -> dict:
+        """
+        during the build, merge the kwargs from the list of args_list,
+        the later kwargs always override the prevous kwargs during the merge.
+        """
         # https://peps.python.org/pep-0584/
         # https://stackoverflow.com/questions/38987/how-do-i-merge-two-dictionaries-in-a-single-expression-in-python
         # merge dictionary python 3.9 |= operator, python 3.8 {**d1, **d2}
         for kwargs in self.args_list:
+            # kwargs override the previous self.kwargs
             self.kwargs = {**self.kwargs, **kwargs}
         return self.kwargs
 
