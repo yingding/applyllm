58fc0eeadbd7a885255331ddc2b4f2f1edf937e1
yingding <yingdingwang@yahoo.de>
2024-02-07 15:08:26 +0100

    updated name of quantization config

--- applyllm/pipelines/config.py
+++ applyllm/pipelines/config.py
@@ -13,16 +13,16 @@ class PipelineConfig:
 
 
 class LocalCausalLMConfig:
-    def __init__(self, quantized: bool = False, model_config: dict = {}, bnb_config: dict = {}, **kwargs):
+    def __init__(self, quantized: bool = False, model_config: dict = {},  quantization_config = None, **kwargs):
         self.quantized = quantized
         self.model_config = model_config
-        self.bnb_config = bnb_config
+        self.quantization_config = quantization_config
         # self.bnb_config = kwargs.get('bnb_config', None)
         # self.model_config = kwargs.get('model_config', None)
 
     def __repr__(self):
         # add !r to format the string representation of the object
-        return f"LocalCausalLMConfig(quantized={self.quantized!r}, model_config={self.model_config!r}, bnb_config={self.bnb_config!r})"
+        return f"LocalCausalLMConfig(quantized={self.quantized!r}, model_config={self.model_config!r}, quantization_config={self.quantization_config!r})"
     
     def __str__(self):
         return repr(self)
