fc7f402d5e95c2344c6637e3db418a2a4ec8e44c
Yingding Wang <yingdingwang@microsoft.com>
2026-04-06 20:08:28 +0200

    update

--- applyllm/accelerators/__init__.py
+++ applyllm/accelerators/__init__.py
@@ -1,6 +1,7 @@
 from .accelerator_utils import (
     DirectorySetting, DIR_MODE_MAP, TokenHelper, AcceleratorHelper,
     AcceleratorStatus, MpsAcceleratorStatus, CudaAcceleratorStatus,
+    XpuAcceleratorStatus,
 )
 
 __all__ = [ DirectorySetting, DIR_MODE_MAP, TokenHelper, AcceleratorHelper,
